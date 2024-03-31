42ac7fd867be5de943f7fa4005b10b09b25ca84e
tahutech <nuralisholikhin19@gmail.com>
2024-03-31 19:32:02 +0700

    upload file

---
+++ backend/database/migrations/2024_02_10_135342_create_drugs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drugs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->integer('dosis')->nullable();
+            $table->string('pabrik')->nullable();
+            $table->integer('jumlah')->nullable();
+            $table->date('expires_at')->nullable();
+            $table->date('input_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drugs');
+    }
+};
